fa80b49703e0b236ac6257dd8933be9f816b7f11
ty875 <tingyue0313@gmail.com>
2025-08-24 21:09:57 -0400

    added .gitignore .gitattributes and files

---
+++ IOS101-Prework/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  IOS101-Prework
+//
+//  Created by kotoko on 8/24/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBAction func changeBackgroundColor(_ sender: Any) {
+        let randomColor = changeColor()
+        view.backgroundColor = randomColor
+    }
+    func changeColor() -> UIColor{
+
+          let red = CGFloat.random(in: 0...1)
+          let green = CGFloat.random(in: 0...1)
+          let blue = CGFloat.random(in: 0...1)
+
+          return UIColor(red: red, green: green, blue: blue, alpha: 0.5)
+      }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ IOS101-PreworkTests/IOS101_PreworkTests.swift
@@ -0,0 +1,17 @@
+//
+//  IOS101_PreworkTests.swift
+//  IOS101-PreworkTests
+//
+//  Created by kotoko on 8/24/25.
+//
+
+import Testing
+@testable import IOS101_Prework
+
+struct IOS101_PreworkTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
